43b97d3a2a3f1bd4eaa6eccf344b219becab5c1e
dilsizk <dilsizk>
2016-10-31 23:23:14 -0400

    lab5 update, receive state seen in our testbench

--- Lab05/project_1/project_1.sim/sim_1/behav/simulate.bat
+++ Lab05/project_1/project_1.sim/sim_1/behav/simulate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2016.2\\bin
-call %xv_path%/xsim mx_rcvr_testbench_behav -key {Behavioral:sim_1:Functional:mx_rcvr_testbench} -tclbatch mx_rcvr_testbench.tcl -log simulate.log
+call %xv_path%/xsim rcvrTEST_behav -key {Behavioral:sim_1:Functional:rcvrTEST} -tclbatch rcvrTEST.tcl -log simulate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
